86275f14c77c542d4fcd2af8029d6e00b3ef40ec
rolando22 <rolando2207@gmail.com>
2023-10-10 11:48:02 -0300

    [ADD] user avatar

--- src/components/TodoCounter/index.tsx
+++ src/components/TodoCounter/index.tsx
@@ -8,7 +8,12 @@ export function TodoCounter() {
 	return (
 		<>
 			<section className='flex justify-between'>
-				<p className='text-lg font-normal opacity-70'>{user.username}</p>
+				<div className='flex justify-center items-center gap-3'>
+					<figure className='w-10 h-10 bg-[#293143] rounded-full'>
+						<img src={user.image} alt={user.username} />
+					</figure>
+					<p className='text-lg font-normal opacity-70'>{user.username}</p>
+				</div>
 				<button 
 					className='text-lg font-normal opacity-70 hover:opacity-100'
 					onClick={handlerOnClickLogout}
